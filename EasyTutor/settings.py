3e58a74714c491a49cb64547f11146df145211e4
Patrick Chaca <40473502+ChacaPatrick@users.noreply.github.com>
2020-03-30 00:14:01 -0400

    Chat Room

--- EasyTutor/settings.py
+++ EasyTutor/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,6 +74,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'EasyTutor.wsgi.application'
+ASGI_APPLICATION = 'EasyTutor.routing.application'
 
 
 # Database
